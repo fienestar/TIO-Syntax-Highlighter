1706e4e89c08c3fc1434e18510c72c26adcf298d
fienestar <fienestar@gmail.com>
2022-06-23 17:29:37 +0900

    feat: syntax highlight

--- syntax-highlight.js
+++ syntax-highlight.js
@@ -1,1 +1,131 @@
+(async () => {
+    const language_table = {
+        'apl': 'apl',
+        'cpp': 'x-c++src',
+        'c': 'x-csrc',
+        'python3': 'x-python',
+        'python2': 'x-python',
+        'javascript': 'javascript',
+        'ecmascript': 'ecmascript',
+        'typescript': 'typescript'
+    }
 
+    function loadCSS(href) {
+        const link = document.createElement('link')
+        link.rel = 'stylesheet'
+        link.type = 'text/css'
+        link.href = href
+        link.crossOrigin = 'anonymous'
+        link.referrerPolicy = 'no-referrer'
+        document.head.appendChild(link)
+        return link
+    }
+
+    function loadJS(src) {
+        return new Promise(resolve => {
+            const script = document.createElement('script')
+            script.src = src
+            script.crossOrigin = 'anonymous'
+            script.referrerPolicy = 'no-referrer'
+            script.addEventListener('load', () => {
+                console.log("load " + src)
+                resolve()
+            })
+            document.body.appendChild(script)
+        })
+    }
+
+    loadCSS('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.4/codemirror.min.css')
+    loadCSS('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.4/theme/material-darker.min.css')
+    await loadJS('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.4/codemirror.min.js')
+    await loadJS('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.4/addon/mode/loadmode.min.js')
+    await loadJS('https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.4/mode/meta.min.js')
+    
+    CodeMirror.modeURL = "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.4/mode/%N/%N.min.js";
+
+    function waitLoadable() {
+        return new Promise(resolve => {
+            const interpreter_toggle = document.getElementById("toggle-interpreter")
+            if (interpreter_toggle.checked === true)
+                resolve()
+            else {
+                function changeListener() {
+                    if (interpreter_toggle.checked === true) {
+                        interpreter_toggle.removeEventListener('change', changeListener)
+                        resolve()
+                    }
+                }
+                interpreter_toggle.addEventListener('change', changeListener)
+            }
+        })
+    }
+
+    await waitLoadable()
+
+    const box = document.createElement('div')
+    box.id = 'code-box'
+    const code = document.getElementById('code')
+    code.parentElement.insertBefore(box, code)
+    box.appendChild(code)
+
+    function getMode() {
+        if (typeof languageId === 'undefined')
+            return 'null'
+        const language = languageId.split('-')[0]
+        const mode = 'text/' + (language_table[language] ?? 'x-' + language)
+        const info = CodeMirror.findModeByMIME(mode);
+        if (info && editor_exists_flag)
+            CodeMirror.autoLoadMode(editor, info.mode)
+        return mode
+    }
+
+    let editor_exists_flag = false
+    const editor = CodeMirror.fromTextArea(code, {
+        theme: 'material-darker',
+        lineNumbers: true,
+        autoCloseBrackets: true,
+        autoCloseTags: true,
+        mode: getMode(),
+        indentUnit: 4,
+        tabSize: 4,
+        extraKeys: {
+            'Tab': () => { },
+        },
+        indentWithTabs: true
+    });
+    editor_exists_flag = true
+    getMode()
+
+    function updateByteCount(code) {
+        const encoding = languages[languageId].encoding
+        const characterCount = countBytes(code, "SBCS");
+        const byteCount = countBytes(code, encoding);
+
+        $("#code-info").textContent = pluralization(characterCount, "char");
+        $("#code-info").textContent += ", " + pluralization(byteCount, "byte") + " (" + encoding + ")";
+    }
+
+    editor.on('change', instance => {
+        updateByteCount(instance.getValue())
+    })
+
+    const run_element = document.getElementById('run')
+    let old_run_onclick = run_element.onclick
+    run_element.onclick = () => {
+        $('#code').value = editor.getValue()
+        old_run_onclick()
+    }
+
+    const lang_example_element = document.getElementById("lang-example")
+    let old_lang_example_onclick = lang_example_element.onclick
+    lang_example_element.onclick = () => {
+        old_lang_example_onclick()
+        editor.setValue($('#code').value)
+    }
+
+    new MutationObserver(() => {
+        editor.setOption('mode', getMode());
+    }).observe(document.getElementById('lang-link'), {
+        attributes: true
+    })
+})()
